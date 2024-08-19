92cc4605bb110c07ea75d92e41e77e4b399416f8
Michael Salmi <miikee713@gmail.com>
2024-08-19 10:45:30 -0400

    Updates license classifier in setup.pu

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url='https://github.com/Cheshire-Labs/orca',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: Other/Proprietary License',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.10',
