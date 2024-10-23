5878cdba77481154f07c69cfac1c090611548dc3
Michael Salmi <miikee713@gmail.com>
2024-10-22 22:18:06 -0400

    Adds method to set logging

--- src/orca/orca_entry.py
+++ src/orca/orca_entry.py
@@ -1,9 +1,6 @@
-import logging
+
 
 from orca.cli.orca_cli import OrcaCmdShell
-
-
-logging.basicConfig(handlers=[logging.StreamHandler()], level=logging.DEBUG)
 
 def main():
     OrcaCmdShell().cmdloop()
