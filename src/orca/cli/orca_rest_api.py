34a80fc799ed6d3636c2226c9254719db531e16c
Michael Salmi <miikee713@gmail.com>
2024-11-11 21:52:04 -0500

    Switches to snake_case to camelCase rest API

--- src/orca/cli/orca_rest_api.py
+++ src/orca/cli/orca_rest_api.py
@@ -1,8 +1,7 @@
-import asyncio
-import json
+import logging
 from logging import Handler, LogRecord
-import threading
 from typing import List
+
 from flask_socketio import SocketIO
 from flask import Flask, jsonify, request, Response
 from flask.helpers import abort
@@ -13,60 +12,60 @@ orca_api = OrcaApi()
 
 app = Flask(__name__)
 
-socketio = SocketIO(app, async_mode='threading')
+socketio = SocketIO(app, async_mode='eventlet')
 
 
 class SocketIOHandler(Handler):
     """A logging handler that emits records with SocketIO."""
     
-    _instance = None
-    _lock = threading.Lock()  # To ensure thread-safety
+    # _instance = None
+    # _lock = threading.Lock()  # To ensure thread-safety
 
-    def __new__(cls, *args, **kwargs):
-        # Use double-checked locking to create a singleton instance
-        if not cls._instance:
-            with cls._lock:
-                if not cls._instance:
-                    cls._instance = super(SocketIOHandler, cls).__new__(cls)
-        return cls._instance
+    # def __new__(cls, *args, **kwargs):
+    #     # Use double-checked locking to create a singleton instance
+    #     if not cls._instance:
+    #         with cls._lock:
+    #             if not cls._instance:
+    #                 cls._instance = super(SocketIOHandler, cls).__new__(cls)
+    #     return cls._instance
 
-    def __init__(self):
-        # Ensure __init__ is only called once
-        if not hasattr(self, '_initialized'):
-            super().__init__()
-            self._initialized = True
+    # def __init__(self):
+    #     # Ensure __init__ is only called once
+    #     if not hasattr(self, '_initialized'):
+    #         super().__init__()
+    #         self._initialized = True
+
+    
 
     def emit(self, record: LogRecord) -> None:
         """Emit a log record with SocketIO."""
-        socketio.emit('log_message', {'data': self.format(record)}, namespace='/logging')
+        socketio.emit('logMessage', {'data': self.format(record)}, namespace='/logging')
+
 
 # WebSocket event to send logs
 @socketio.on('connect', '/logging')
 def handle_connect():
     print("Client connected")
-    socketio_logging: Handler = SocketIOHandler()
-    orca_api.set_logging_destination(socketio_logging, "INFO")
-    socketio.emit('log_message', {'data': 'Connected to Orca Server'})
+    # socketio_logging: Handler = SocketIOHandler()
+    # orca_api.set_logging_destination(socketio_logging, "INFO")
+    # socketio.emit('logMessage', {'data': 'Socket Logging Connected to Orca Server'})
 
 @socketio.on('disconnect', '/logging')
 def handle_disconnect():
     print("Client disconnected")
 
-
-
-
 @app.route('/load', methods=['POST'])
 def load() -> Response:
     data = request.get_json()
-    config_file = data.get("config_file")
+    config_file = data.get("configFile")
     orca_api.load(config_file)
     return jsonify({"message": "Configuration loaded successfully."})
 
 @app.route('/init', methods=['POST'])
 def init() -> Response:
     data = request.get_json()
-    config_file = data.get("config_file")
-    resource_list = data.get("resource_list", [])
+    config_file = data.get("configFile")
+    resource_list = data.get("resourceList", [])
     options = data.get("options", {})
     orca_api.init(config_file=config_file, resource_list=resource_list, options=options)
     return jsonify({"message": "Initialization complete."})
@@ -74,28 +73,28 @@ def init() -> Response:
 @app.route('/run_workflow', methods=['POST'])
 def run_workflow() -> Response:
     data = request.get_json()
-    workflow_name = data.get("workflow_name")
-    config_file = data.get("config_file", None)
+    workflow_name = data.get("workflowName")
+    config_file = data.get("configFile", None)
     options = data.get("options", {})
     workflow_id = orca_api.run_workflow(workflow_name=workflow_name, 
                                         config_file=config_file, 
                                         options=options)
-    return jsonify({"workflow_id": workflow_id})
+    return jsonify({"workflowId": workflow_id})
 
 @app.route('/run_method', methods=['POST'])
 def run_method() -> Response:
     data = request.get_json()
-    method_name = data.get("method_name")
-    start_map_json = data.get("start_map")
-    end_map_json = data.get("end_map")
-    config_file = data.get("config_file", None)
+    method_name = data.get("methodName")
+    start_map_json = data.get("startMap")
+    end_map_json = data.get("endMap")
+    config_file = data.get("configFile", None)
     options = data.get("options", {})
     method_id = orca_api.run_method(method_name=method_name, 
                                     start_map_json=start_map_json, 
                                     end_map_json=end_map_json,
                                     config_file=config_file, 
                                     options=options)
-    return jsonify({"method_id": method_id})
+    return jsonify({"methodId": method_id})
 
 @app.route('/get_workflow_recipes', methods=['GET'])
 def get_workflow_recipes() -> Response:
@@ -106,19 +105,20 @@ def get_workflow_recipes() -> Response:
         for tr in r.thread_templates:
             thread_recipes[tr.name] = {
                 "name": tr.name,
-                "start_location": tr.start_location.name,
-                "end_location": tr.end_location.name,
-                "labware_template": tr.labware_template.name
+                "startLocation": tr.start_location.name,
+                "endLocation": tr.end_location.name,
+                "labwareTemplate": tr.labware_template.name
             }
         dict_recipes[name] = {
             "name": r.name,
-            "thread_recipes": thread_recipes,
+            "threadRecipes": thread_recipes,
         }
 
-    return jsonify({"workflow_recipes": dict_recipes})
+    return jsonify({"workflowRecipes": dict_recipes})
 
 @app.route('/test', methods=['GET'])
 def test() -> Response:
+    logging.info("Test pinged")
     return jsonify({"status": "route reachable"})
 
 @app.route('/get_method_recipes', methods=['GET'])
@@ -131,12 +131,12 @@ def get_method_recipes() -> Response:
             "inputs": [labware.name for labware in r.inputs],
             "outputs": [labware.name for labware in r.outputs]
         }
-    return jsonify({"method_recipes": dict_recipes})
+    return jsonify({"methodRecipes": dict_recipes})
 
 
 @app.route('/get_method_recipe_input_labwares', methods=['GET'])
 def get_method_recipe_input_labwares() -> Response:
-    method_name = request.args.get('method_name')
+    method_name = request.args.get('methodName')
     if not method_name:
         abort(400, 'Method name is required')
     method_recipe = orca_api.get_method_recipes()[method_name]
@@ -150,11 +150,11 @@ def get_method_recipe_input_labwares() -> Response:
             labware_inputs.append(labware.name)
         else:
             raise TypeError(f"Labware {labware} is not a recognized labware template type")
-    return jsonify({"input_labwares": labware_inputs})
+    return jsonify({"inputLabwares": labware_inputs})
 
 @app.route('/get_method_recipe_output_labwares', methods=['GET'])
 def get_method_recipe_output_labwares() -> Response:
-    method_name = request.args.get('method_name')
+    method_name = request.args.get('methodName')
     if not method_name:
         abort(400, 'Method name is required')
     method_recipe = orca_api.get_method_recipes()[method_name]
@@ -168,12 +168,12 @@ def get_method_recipe_output_labwares() -> Response:
             labware_outputs.append(labware.name)
         else:
             raise TypeError(f"Labware {labware} is not a recognized labware template type")
-    return jsonify({"output_labwares": labware_outputs})
+    return jsonify({"outputLabwares": labware_outputs})
 
 @app.route('/get_labware_recipes', methods=['GET'])
 def get_labware_recipes() -> Response:
     recipes = orca_api.get_labware_recipes()
-    return jsonify({"labware_recipes": recipes})
+    return jsonify({"labwareRecipes": recipes})
 
 @app.route('/get_running_workflows', methods=['GET'])
 def get_running_workflows() -> Response:
@@ -213,35 +213,27 @@ def stop() -> Response:
 @app.route('/get_installed_drivers_info', methods=['GET'])
 def get_installed_drivers_info() -> Response:
     drivers = orca_api.get_installed_drivers_info()
-    return jsonify({"installed_drivers_info": drivers})
+    return jsonify({"installedDriversInfo": drivers})
 
 @app.route('/get_available_drivers_info', methods=['GET'])
 def get_available_drivers_info() -> Response:
     drivers = orca_api.get_available_drivers_info()
-    return jsonify({"available_drivers_info": drivers})
+    return jsonify({"availableDriversInfo": drivers})
 
 @app.route('/install_driver', methods=['POST'])
 def install_driver() -> Response:
     data = request.get_json()
-    driver_name = data.get("driver_name")
-    driver_repo_url = data.get("driver_repo_url")
+    driver_name = data.get("driverName")
+    driver_repo_url = data.get("driverRepoUrl")
     orca_api.install_driver(driver_name, driver_repo_url)
     return jsonify({"message": f"Driver '{driver_name}' installed successfully."})
 
 @app.route('/uninstall_driver', methods=['POST'])
 def uninstall_driver() -> Response:
     data = request.get_json()
-    driver_name = data.get("driver_name")
+    driver_name = data.get("driverName")
     orca_api.uninstall_driver(driver_name)
     return jsonify({"message": f"Driver '{driver_name}' uninstalled successfully."})
-
-@app.route('/set_logging_destination', methods=['POST'])
-def set_logging_destination() -> Response:
-    data = request.get_json()
-    destination = data.get("destination")
-    logging_level = data.get("logging_level", "info")
-    orca_api.set_logging_destination(destination, logging_level)
-    return jsonify({"message": "Logging destination updated successfully."})
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -256,6 +248,7 @@ def shutdown():
 
 if __name__ == "__main__":
     # app.run(host="127.0.0.1", port=5000, debug=True)
+    logging.basicConfig(handlers=[logging.StreamHandler(), SocketIOHandler()], level=logging.DEBUG)
     socketio.run(app, host="127.0.0.1", port=5000, debug=True, allow_unsafe_werkzeug=True)
     # asyncio.run(socketio.run(logging_app, host="127.0.0.1", port=5001, debug=True, allow_unsafe_werkzeug=True))
     
