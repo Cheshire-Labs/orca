fa2cefb5527972919f64d946272fae7913d1c455
Michael Salmi <miikee713@gmail.com>
2025-06-25 21:42:08 -0400

    Major  cleanup of unused legacy code

--- src/orca/yml_config_builder/special_yml_parsing.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def is_dynamic_yaml(value: str) -> bool:
-    return re.search(r"\$\{([^}]+)\}", value) is not None
-
-def get_dynamic_yaml_keys(value: str) -> list:
-    return re.findall(r"\$\{([^}]+)\}", value)
