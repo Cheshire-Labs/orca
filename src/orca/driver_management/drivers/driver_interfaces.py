a2d95e94fcc79a597e4a881b0668f6f02b8ac493
Michael Salmi <miikee713@gmail.com>
2025-09-29 13:46:03 -0700

    Converts things from protocols back to ABCs

--- src/orca/driver_management/drivers/driver_interfaces.py
+++ src/orca/driver_management/drivers/driver_interfaces.py
@@ -1,53 +1,58 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List
-from typing_extensions import Protocol, runtime_checkable
+
 
 from orca.resource_models.resource_extras.teachpoints import Teachpoint
 
 
-
-
-
-@runtime_checkable
-class BaseDriver(Protocol):
+class BaseDriver(ABC):
+    @abstractmethod
     async def initialize(self) -> None:
         """Setup the driver."""
         ...
     
     @property
+    @abstractmethod
     def is_initialized(self) -> bool:
         """Returns whether the driver is initialized or not."""
         ...
 
+    @abstractmethod
     async def open(self) -> None:
         """Open driver door."""
         ...
 
+    @abstractmethod
     async def close(self) -> None:
         """Close driver door."""
         ...
 
 
-@runtime_checkable
-class IShakerDriver(BaseDriver, Protocol):
 
+class IShakerDriver(BaseDriver, ABC):
+
+    @abstractmethod
     async def stop(self) -> None:
         """Stop the shaker backend."""
         ...
 
     @property
+    @abstractmethod
     def supports_locking(self) -> bool:
         """Check if the shaker supports locking the plate"""
         ...
 
+    @abstractmethod
     async def unlock_plate(self) -> None:
         """Unlock the plate"""
         ...
 
+    @abstractmethod
     async def lock_plate(self):
         """Lock the plate"""
         ...
 
+    @abstractmethod
     async def shake(self, speed: float, duration: float) -> None:
         """Shake the shaker at the given speed
 
@@ -57,104 +62,121 @@ class IShakerDriver(BaseDriver, Protocol):
         """
         ...
 
+    @abstractmethod
     async def stop_shaking(self) -> None:
         """Stop shaking"""
         ...
 
-@runtime_checkable
-class ISealerDriver(BaseDriver, Protocol):
 
+class ISealerDriver(BaseDriver, ABC):
+
+    @abstractmethod
     async def seal(self, temperature: int, duration: float) -> None:
         """Seal at specified temperature and duration."""
         ...
 
+    @abstractmethod
     async def set_temperature(self, temperature: float) -> None:
         """Set the temperature."""
         ...
 
+    @abstractmethod
     async def get_temperature(self) -> float:
         """Get the current temperature."""
         ...
 
 
-@runtime_checkable
-class ITempSettableDriver(Protocol):
+
+class ITempSettableDriver(ABC):
+    
+    @abstractmethod
     async def set_temperature(self, temperature: float) -> None:
         """Set the temperature of the device."""
         ...
 
-@runtime_checkable
-class ITempGettableDriver(Protocol):
+
+class ITempGettableDriver(ABC):
+    @abstractmethod
     async def get_temperature(self) -> float:
         """Get the current temperature of the device."""
         ...
 
-@runtime_checkable
-class IProtocolRunnerDriver(BaseDriver, Protocol):
+
+class IProtocolRunnerDriver(BaseDriver, ABC):
+    @abstractmethod
     async def run_protocol(self, protocol_filepath: str, params: Dict[str, Any]) -> None:
         """Execute a protocol run command."""
         ...    
    
-@runtime_checkable
-class ICentrifugeDriver(BaseDriver, Protocol):
 
+class ICentrifugeDriver(BaseDriver, ABC):
+    @abstractmethod
     async def centrifuge(self, g: float, duration: float) -> None:
         """Spin the centrifuge at a specified speed for a specified duration."""
         ...  
 
-@runtime_checkable
-class IReaderDriver(BaseDriver, Protocol):
+
+class IReaderDriver(BaseDriver, ABC):
+    @abstractmethod
     async def read(self, protocol_filepath: str, output_filepath: str) -> None:
         """Read data using the specified protocol and save results to a file."""
         ...
 
-@runtime_checkable
-class IDelidderDriver(BaseDriver, Protocol):
+
+class IDelidderDriver(BaseDriver, ABC):
+    @abstractmethod
     async def delid(self) -> None:
         """Remove the lid from the specified labware."""
         ...
 
-@runtime_checkable
-class ITransporterDriver(Protocol):
+
+class ITransporterDriver(ABC):
 
     @property
+    @abstractmethod
     def name(self) -> str:
         """Returns the name of the transporter."""
         ...
 
+    @abstractmethod
     async def initialize(self) -> None:
         """Setup the driver."""
         ...
     
     @property
+    @abstractmethod
     def is_initialized(self) -> bool:
         """Returns whether the driver is initialized or not."""
         ...
 
+    @abstractmethod
     async def pick(self, position_name: str, labware_type: str) -> None:
         ...
 
+    @abstractmethod
     async def place(self, position_name: str, labware_type: str) -> None:
         ...
 
+    @abstractmethod
     def get_teachpoints(self) -> List[Teachpoint]:
         ...
 
+    @abstractmethod
     def load_teachpoints(self, teachpoints: List[Teachpoint]) -> None:
         ...
 
-@runtime_checkable
-class IStorageDriver(BaseDriver, Protocol):
+
+class IStorageDriver(BaseDriver, ABC):
     pass
 
-@runtime_checkable
-class IPlateWasherDriver(IProtocolRunnerDriver, Protocol):
+
+class IPlateWasherDriver(IProtocolRunnerDriver, ABC):
     pass
 
-@runtime_checkable
-class ILiquidHandlerDriver(IProtocolRunnerDriver, Protocol):
+
+class ILiquidHandlerDriver(IProtocolRunnerDriver, ABC):
     pass
 
-@runtime_checkable
-class IWasteDriver(IStorageDriver, Protocol):
+
+class IWasteDriver(IStorageDriver, ABC):
     pass
