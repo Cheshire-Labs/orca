15376a23f6a05c681417d4f9e1cc5a2a028108c9
Michael Salmi <miikee713@gmail.com>
2025-12-08 12:25:52 -0800

    Update teachpoint coordinates for red_pad and black_pad in precise_flex test and update export of devices.

--- src/orca/sdk/devices.py
+++ src/orca/sdk/devices.py
@@ -5,6 +5,8 @@ from orca.resource_models.transporter import Transporter
 from orca.driver_management.drivers.a4s_sealer import A4SSealer
 from orca.devices.sealer import Sealer
 from orca.devices.shaker import Shaker
+from orca.devices.centrifuge import Centrifuge
+from orca.driver_management.drivers.human_transfer import HumanTransfer
 
 __all__ = [
     "Device",
@@ -13,5 +15,7 @@ __all__ = [
     "A4SSealer",
     "Venus",
     "Sealer",
-    "Shaker"
+    "Shaker",
+    "Centrifuge",
+    "HumanTransfer"
 ]
